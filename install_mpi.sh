8eafdfec92262745ebdffa83fe977f7db4f2ff5c
bwal213 <36938110+bwal213@users.noreply.github.com>
2018-09-30 22:53:01 -0400

    Added yum update to install_mpi.sh
    Added:
    sudo yum -y update
    on line 3
    -Ben Walker

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+sudo yum -y update
 sudo yum -y group install "Development Tools"
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
