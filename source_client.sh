1e3f0c8d3ddb741a5432969b460e3b3ccfa9efe5
beccabuoni <38893323+beccabuoni@users.noreply.github.com>
2018-10-18 18:19:45 -0400

    Add files via upload

---
+++ source_client.sh
@@ -0,0 +1,3 @@
+echo "export PATH='$PATH:/software/openmpi/3.1.2/bin'" >> /users/BW840606/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/'" >> /users/BW840606/.bashrc
+source /users/BW840606/.bashrc
